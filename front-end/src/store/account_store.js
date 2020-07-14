d88cb123f1701ade10c899bad19ede7bdecff100
Dakota Logan <xeliusone@gmail.com>
2020-07-13 21:42:43 -0600

    SOFetch method design complete (for ease of making and reading fetch requests rather than using fetch calls in every piece of code that needs to do so) lol.

--- front-end/src/store/account_store.js
+++ front-end/src/store/account_store.js
@@ -4,7 +4,7 @@ const mod = {
 	} ),
 	mutations: {
 		login ( state, data ) {
-			Axios.post()
+		
 			
 		},
 		register ( state, data ) {
@@ -14,48 +14,12 @@ const mod = {
 	},
 	actions: {
 		login ( { commit, dispatch }, creds ) {
-			fetch()
+			dispatch("SOFetch", )
 		},
 		register ( { commit, dispatch }, account ) {
 		},
 		logout () {
 		},
-		//helpers
-		async SOFetch ( { commit, dispatch }, w, m, d ) {
-			let where = w,
-				method,
-				data = d;
-			
-			//? Check the type of fetch request.
-			switch (m) {
-				case "c" :
-					method = "post";
-					break;
-				case "r" :
-					method = "get";
-					break;
-				case "u" :
-					method = "put";
-					break;
-				case "d" :
-					method = "delete";
-					break;
-				default:
-					throw new Error("That is not a crud operation! (use lower case letters: c, r, u, or d)");
-					break;
-			}
-			
-			try {
-				let RR = await fetch(where, {
-					method: method,
-					body: data
-				});
-				
-				return RR;
-			} catch (e) {
-				return e;
-			}
-		}
 	},
 	getters: {
 		isLogged ( state ) {
--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -12,7 +12,50 @@ export default new Vuex.Store({
 	},
 	mutations: {},
 	actions: {
-		getRecipes ( { commit, dispatch },  ) {
+		getRecipes ( { commit, dispatch }, ) {},
+		/**
+		 * @method
+		 * The 1, 2, and 3 arguments should be as follows: 1| where |2| method |3| data
+		 * @async
+		 * @param w {string} The address to fetch from. (/recipes, /user/account, etc.)
+		 * @param m {string} - Which method (crud operations) to use. Uses the letter of the operation from CRUD
+		 * @param d {object} - The data to pass on (no formatting of the data is done between this method and the api).
+		 * @returns {Promise<Response|*>}
+		 */
+		async SOFetch ( { commit, dispatch }, w, m, d ) {
+			let where = w,
+				method,
+				data = d;
+			
+			//? Check the type of fetch request.
+			switch (m.toLowerCase()) {
+				case "c" :
+					method = "post";
+					break;
+				case "r" :
+					method = "get";
+					break;
+				case "u" :
+					method = "put";
+					break;
+				case "d" :
+					method = "delete";
+					break;
+				default:
+					throw new Error("That is not a crud operation! (use lower case letters: c, r, u, or d)");
+					break;
+			}
+			
+			try {
+				let RR = await fetch(where, {
+					method: method,
+					body: data
+				});
+				
+				return RR;
+			} catch (e) {
+				return e;
+			}
 		}
 	},
 	modules: {
